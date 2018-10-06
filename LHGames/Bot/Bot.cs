97865417d946aba91a1c567f17365e836582ecbb
jeepee97 <jeanphilippe.anctil@hotmail.com>
2018-10-06 12:10:38 -0400

    ajout class PathNoeud et debut pathFinding

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -4,8 +4,61 @@ using LHGames.Helper;
 
 namespace LHGames.Bot
 {
+    class PathNoeud
+    {
+        int x;
+        int y;
+        int gCost;
+        int hCost;
+        int fCost;
+        PathNoeud parent;
+
+        public PathNoeud(int x_, int y_, int gCost_, int hCost_, PathNoeud parent_)
+        {
+            x = x_;
+            y = y_;
+            gCost = gCost_;
+            hCost = hCost_;
+            parent = parent_;
+            setFCost();
+        }
+
+        void setFCost()
+        {
+            fCost = hCost + gCost;
+        }
+
+        public int getFCost()
+        {
+            return fCost;
+        }
+
+        public int getHCost()
+        {
+            return hCost;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public int getGCost()
+        {
+            return gCost;
+        }
+
+    }
+
     internal class Bot
     {
+
+
         internal IPlayer PlayerInfo { get; set; }
         private int _currentDirection = 1;
 
@@ -28,6 +81,10 @@ namespace LHGames.Bot
         /// <returns>The action you wish to execute.</returns>
         internal string ExecuteTurn(Map map, IEnumerable<IPlayer> visiblePlayers)
         {
+            PathNoeud playerPosition = new PathNoeud(PlayerInfo.Position.X, PlayerInfo.Position.Y, 0, 0, null);
+            PathNoeud minePosition = new PathNoeud()
+            List<PathNoeud> path =  trouverPathMine(playerPosition, minePosition);
+
             // TODO: Implement your AI here.
             if (map.GetTileAt(PlayerInfo.Position.X + _currentDirection, PlayerInfo.Position.Y) == TileContent.Wall)
             {
@@ -45,6 +102,57 @@ namespace LHGames.Bot
         internal void AfterTurn()
         {
         }
+
+        static List<PathNoeud> trouverPathMine(PathNoeud start, PathNoeud end)
+        {
+            List<PathNoeud> openSet = new List<PathNoeud>();
+            List<PathNoeud> closedSet = new List<PathNoeud>();
+            List<PathNoeud> path = new List<PathNoeud>();
+            openSet.Add(start);
+            while (openSet.Count > 0)
+            {
+                // choisi le prochain noeud a evaluer
+                PathNoeud next = openSet[0];
+                foreach(PathNoeud n in openSet)
+                {
+                    if (next.getFCost() > n.getFCost() || (next.getFCost() == n.getFCost() && next.getHCost() < n.getHCost()))
+                    {
+                        next = n;
+                    }
+                }
+                // trouve les voisins
+                List<PathNoeud> neighbours = getNeighbours(n, end);
+
+            }
+
+            return null;
+        }
+
+        static List<PathNoeud> getNeighbours(PathNoeud n, PathNoeud end)
+        {
+            List<PathNoeud> neighbours = new List<PathNoeud>();
+
+            for (int i = -1; i <= 1; i += 2)
+            {
+                int posX = n.getX();
+                int posY = n.getY();
+                PathNoeud tampon = new PathNoeud(posX, posY, n.getGCost() + 1, )
+
+                neighbours.Add()
+            }
+
+
+            return neighbours;
+        }
+
+        static int calculerHCost(PathNoeud n, PathNoeud end)
+        {
+            int hCost = 0;
+            int n.getX();
+
+            return hCost;
+        }
+
     }
 }
 
